48ff41cd695f87bf239dd10ff11a4534103dcf58
Daniel.Brahano <daniel431994@gmail.com>
2024-08-16 17:49:44 +0300

    Added STM headers to project, toggled LED using ST header file, and added a project to turn off the LED by pressing a button.

---
+++ 4_gpio_input/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/core_cm4.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/core_cm4.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_version.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/STM32CubeIDE/MCU1-Course/MCU1/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
